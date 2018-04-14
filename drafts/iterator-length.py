b2496a0b54cf1aee03fdae1e62897842bf454e51
Reece Hart <reecehart@gmail.com>
2018-04-14 13:12:48 -0700

    added new hugo content

---
+++ drafts/iterator-length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.5
+
+# https://stackoverflow.com/questions/3345785/getting-number-of-elements-in-an-iterator-in-python
+
+# Q: Is it possible to know the length of an iterator without iterating through it?
+
+# A: In general, no.  Iterators are essential a stream of information,
+# potentially of infinite length.
+
+
+
+
+class 
